b839a2afc7c5aca4cff7e57956b84c550cb2a3cd
linucksrox <ericdalyblog@dalydays.com>
2018-09-17 08:56:03 -0400

    Wire button to second screen

--- app/src/main/java/com/dalydays/android/navigation/FirstDestination.kt
+++ app/src/main/java/com/dalydays/android/navigation/FirstDestination.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first_destination.*
 
 class FirstDestination : Fragment() {
 
@@ -13,6 +15,12 @@ class FirstDestination : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first_destination, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        go_to_second_btn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_firstDestination_to_secondDestination))
     }
 
 
